632b205a424699333781d05ab2d6229a36ffc929
Saskia110 <Saskia.Funk@uni-duesseldorf.de>
2016-01-05 19:09:21 +0100

    neu

--- lorenz.cxx
+++ lorenz.cxx
@@ -26,20 +26,20 @@ int main(){
     
     const int a = 10;
     const int b = 28;
-    const double c = 8/3;
+    const double c = 8/3.0;
     // einmal ausgeben für Anfangsbedingungen
     cout <<  0 << "\t" << x << "\t" << y << "\t"<< z << endl;
     
-    for(int i=0; i<N; i++){
+    for(int i=0; i<=N; i++){
         lorenz(k1, a, b, c, x, y, z);// jetzt wurde k1 bestimmt, dass nun für alle k1-k4 machen, einzelne        //
         lorenz(k2, a, b, c, x+dt/2*k1[0], y+dt/2*k1[1], z+dt/2*k1[2]); //Abhängikeiten beachten
         lorenz(k3, a, b, c, x+dt/2*k2[0], y+dt/2*k2[1], z+dt/2*k2[2]);
         lorenz(k4, a, b, c, x+dt*k3[0],   y+dt*k3[1],   z+dt*k3[2] );
         
         //neue Werte bestimmen
-        x = x + dt*(1/6)*(k1[0]+ 2*k2[0]+2*k3[0]+ k4[0]);
-        y = y + dt*(1/6)*(k1[1]+ 2*k2[1]+2*k3[1]+ k4[1]);
-        z = z + dt*(1/6)*(k1[2]+ 2*k2[2]+2*k3[2]+ k4[2]);
+        x += x + dt*(1/6.0)*(k1[0]+ 2*k2[0]+2*k3[0]+ k4[0]);
+        y += y + dt*(1/6.0)*(k1[1]+ 2*k2[1]+2*k3[1]+ k4[1]);
+        z += z + dt*(1/6.0)*(k1[2]+ 2*k2[2]+2*k3[2]+ k4[2]);
         
         cout << i*dt << "\t" << x << "\t" << y << "\t"<< z << endl;
     
